10bdcc853f753e4c8ba39664bcfdb13905d70bbd
GrPaladino <paladino.graziano@gmail.com>
2024-03-15 16:20:07 +0100

    Feat: add header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
+<head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="ie=edge" http-equiv="X-UA-Compatible">
     <title>Document</title>
 
     @vite('resources/js/app.js')
-  </head>
+</head>
 
-  <body>
+<body>
+    @include('partials.header')
     <h1>Laravel with Bootstrap + Vite</h1>
-  </body>
+    @include('partials.footer')
+</body>
 
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,55 @@
+<header>
+    <div class="upper-header">
+        <div class="container">
+            <ul>
+                <li><a href="#">DC POWER VISA</a></li>
+                <li><a href="#">ADDITIONAL DC SITES</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="bottom-header container py-3">
+
+        <div class="logo">
+            <img src=" {{ Vite::asset("resources/images/dc-logo.png")}} " alt=""></div>
+        <nav class="navbar">
+            <ul>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">MOVIES</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+                <li>
+                    <a href="#">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="#">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="#">FANS</a>
+                </li>
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+                <li>
+                    <a href="#">SHOP</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="search-bar">
+            <input type="text" placeholder="Search...">
+        </div>
+
+
+    </div>
+</header>
