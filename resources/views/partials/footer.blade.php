cf6eda5088f2f8c732b5952ece3386cdbd05223c
GrPaladino <paladino.graziano@gmail.com>
2024-03-15 17:31:26 +0100

    Feat: added upper footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,129 @@
 <footer>
-    footer
+    <div class="upper-footer">
+        <div class="container">
+            <div class="left">
+
+                <div class="lists">
+                    <div class="list">
+                        <div class="upper">
+                            <h6>DC COMICS</h6>
+
+                            <ul>
+                                <li>
+                                    <a href="#">CHARACTERS</a>
+                                </li>
+                                <li>
+                                    <a href="#">COMICS</a>
+                                </li>
+                                <li>
+                                    <a href="#">MOVIES</a>
+                                </li>
+                                <li>
+                                    <a href="#">TV</a>
+                                </li>
+                                <li>
+                                    <a href="#">GAMES</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">VIDEOS</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">NEWS</a>
+                                </li>
+
+                            </ul>
+                        </div>
+                        <div class="bottom mt-5">
+                            <h6>SHOP</h6>
+
+                            <ul>
+                                <li>
+                                    <a href="#">SHOP DC</a>
+                                </li>
+                                <li>
+                                    <a href="#">SHOP DC COLLECTIBLES</a>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="list">
+                        <h6>DC</h6>
+
+                        <ul>
+                            <li>
+                                <a href="#">Terms Of Use</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy Policy(New)</a>
+                            </li>
+                            <li>
+                                <a href="#">Ad Choices</a>
+                            </li>
+                            <li>
+                                <a href="#">Advertising</a>
+                            </li>
+                            <li>
+                                <a href="#">Jobs</a>
+                            </li>
+                            <li>
+                                <a href="#">Subscriptions</a>
+                            </li>
+                            <li>
+                                <a href="#">Talent Workshops</a>
+                            </li>
+
+                            <li>
+                                <a href="#">CPSC Certificates</a>
+                            </li>
+                            <li>
+                                <a href="#">Ratings</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop Help</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <div class="list">
+                        <h6>SITES</h6>
+
+                        <ul>
+                            <li>
+                                <a href="#">DC</a>
+                            </li>
+                            <li>
+                                <a href="#">MAD Magazine</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Kids</a>
+                            </li>
+                            <li>
+                                <a href="#">Dc Universe</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Power Visa</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+
+                <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, ratione, rem fuga unde consectetur mollitia cupiditate sed id odit consectetur adipisicing elit. Nemo, ratione, rem fuga unde consectetur mollitia cupiditate sed id odit.</p>
+
+            </div>
+            <div class="rigth">
+                <div class="img">
+                    <img src="{{Vite::asset('resources/images/dc-logo-bg.png')}}" alt=""></div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom-footer"></div>
+
 </footer>
