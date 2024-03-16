e7a2a606d6c46d5c42ec85df363327d984c933dc
GrPaladino <paladino.graziano@gmail.com>
2024-03-16 17:43:36 +0100

    Feat: add bottom comic description section

--- resources/views/partials/comic_demo.blade.php
+++ resources/views/partials/comic_demo.blade.php
@@ -28,7 +28,72 @@
             </div>
         </div>
     </div>
-    <div class="bottom-comic">
-        bottom</div>
+    <div class="bottom-comic pt-5">
+        <div class="wrapper">
+            <div class="bottom-left col-6">
+                <h3>Talent</h3>
+
+                <table class="table mt-3">
+
+                    <tbody>
+                        <tr>
+                            <th class="col-3" scope="row">Art by:</th>
+                            <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aperiam excepturi, dolor quo fugit deleniti similique accusamus iusto. Perspiciatis dolorum temporibus.</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Written by:</th>
+                            <td>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae doloribus, saepe repellendus tempore qui excepturi fuga placeat rem architecto nulla reprehenderit numquam.</td>
+                    </tbody>
+                </table>
+            </div>
+            <div class="bottom-right col-6 ps-2">
+                <h3>Specs</h3>
+
+                <table class="table mt-3">
+
+                    <tbody>
+                        <tr>
+                            <th class="col-3" scope="row">Series:</th>
+                            <td>Action Comics</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">U.S. Price:</th>
+                            <td class="color">$19.99</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">On Sale Date</th>
+                            <td class="color">October 02 2018</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="bottom mt-4">
+            <div class="wrapper">
+                <div class="icon digital">
+                    <p>DIGITAL COMICS</p>
+                    <img src="{{Vite::asset('resources/images/buy-comics-digital-comics.png')}}" alt="">
+                </div>
+                <div class="icon shop">
+                    <p>SHOP DC</p>
+                    <img src="{{Vite::asset('resources/images/buy-comics-merchandise.png')}}" alt="">
+
+                </div>
+                <div class="icon comic">
+                    <p>COMIC SHOP LOCATOR</p>
+                    <img src="{{Vite::asset('resources/images/buy-comics-shop-locator.png')}}" alt="">
+
+
+                </div>
+                <div class="icon subscrition">
+                    <p>SUBSCRIPTION</p>
+                    <img src="{{Vite::asset('resources/images/buy-comics-subscriptions.png')}}" alt="">
+
+
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
